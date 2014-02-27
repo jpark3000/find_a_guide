f84e634c706a85eafcfb750f66566f4123c18195
Steven Spiel <stevenspiel@gmail.com>
2014-02-27 10:38:39 -0600

    create basic user model testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe User do
-
+  it {is_expected.to validate_presence_of :first_name}
+  it {is_expected.to validate_presence_of :last_name}
+  it {is_expected.to validate_presence_of :email}
 end
