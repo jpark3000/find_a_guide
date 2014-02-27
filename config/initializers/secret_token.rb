47acc497fd0e85e1fd7fb5e69f1222fa1d36acd7
Justin Park <jpark3000@gmail.com>
2014-02-27 09:32:32 -0600

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FindAGuide::Application.config.secret_key_base = '7e397394c33eb0a7da165e51a78075b7a67137ee6d895898329c8ad83b81e06a253d4a86a6e0b4865c555c6c837425731475bd37798e97652648efcd048e7d48'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FindAGuide::Application.config.session_store :cookie_store, key: '_find_a_guide_session'
