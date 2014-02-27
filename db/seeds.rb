95f3dcddf7018f5bbf5ba8e3bff8b5399a690704
Steven Spiel <stevenspiel@gmail.com>
2014-02-27 17:24:09 -0600

    change femail to female

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ user = User.create!(first_name: "user", last_name: 'user', email: 'email@email.c
                    profile_pic: 'pic', bio: 'this is my bio', gender: 'male',
                    phone: '3127592048', age: 25, tagline: "It's gonna be a good time.")
 
-gender = ['male','femail']
+gender = ['male','female']
 
 50.times do
   User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, 
