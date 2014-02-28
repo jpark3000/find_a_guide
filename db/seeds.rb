db365b91200875a5318f68ef0b8312d6d2173b8c
JuLu <junting.lu@gmail.com>
2014-02-27 18:04:12 -0600

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,8 +18,7 @@ l3 = Language.create!(language: "Korean")
 l4 = Language.create!(language: "German")
 
 
-user = User.create!(first_name: "user", last_name: 'user', email: 'email@email.com',
-                   profile_pic: 'pic', bio: 'this is my bio', gender: 'male',
+user = User.create!(first_name: "user", last_name: 'user', email: 'email@email.com', bio: 'this is my bio', gender: 'male',
                    phone: '3127592048', age: 25, tagline: "It's gonna be a good time.",
                    is_ambassador: true)
 
@@ -27,9 +26,9 @@ gender = ['male','female']
 boolean = [true,false]
 
 50.times do
-  User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, 
-               email: Faker::Internet.email, profile_pic: 'pic', bio: Faker::Lorem.sentences.join(" "), 
-               gender: gender.sample, phone: Faker::PhoneNumber.cell_phone, age: (18..60).to_a.sample, 
+  User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,
+               email: Faker::Internet.email, bio: Faker::Lorem.sentences.join(" "),
+               gender: gender.sample, phone: Faker::PhoneNumber.cell_phone, age: (18..60).to_a.sample,
                tagline: Faker::Commerce.product_name, is_ambassador: boolean.sample)
 end
 
