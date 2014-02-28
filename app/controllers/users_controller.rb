b80ba48a3a48e0dcbe417796ca518faeb3c2a24c
Steven Spiel <stevenspiel@gmail.com>
2014-02-28 12:09:12 -0600

    add review section to visitor side of dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,5 +16,6 @@ class UsersController < ApplicationController
   def dashboard
     @user = current_user
     @visitor_tours = @user.visitor_meetups
+    @incomplete_reviews = @user.empty_reviews(:visitor)
   end
 end
