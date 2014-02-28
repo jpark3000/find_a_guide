ccdc42e211cffcbfcce6fd9f7a7771bbc1225994
Steven Spiel <stevenspiel@gmail.com>
2014-02-28 09:28:32 -0600

    render meetups#show view

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -3,6 +3,11 @@ class MeetupsController < ApplicationController
   end
 
   def show
+    @meetup = current_user.ambassador_meetups.find(params[:id])
+    @ambassador = @meetup.ambassador
+    @visitor = @meetup.visitor
+    @ambassador_attributes = @ambassador.open_information
+    @visitor_attributes = @visitor.open_information
   end
 
   def edit
