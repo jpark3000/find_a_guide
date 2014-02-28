3ac0f24ad0f6918192c86f49464d8a4806be7744
CharlieTruong <charlie.m.truong@gmail.com>
2014-02-28 13:43:44 -0600

    finalized users#show route and view

--- spec/features/users_features_spec.rb
+++ spec/features/users_features_spec.rb
@@ -5,14 +5,19 @@ feature 'User viewing and editing profile data' do
   let(:user) {User.create(first_name: "John", last_name: "Smith", email: "john@dbc.com")}
 
   before(:each) do
-    ApplicationController.any_instance.stub(:current_user).and_return(user)
     page.set_rack_session(:user_id => user.id)
   end
 
   context "on users#show" do
     it "allows a logged in user to see their information" do
-      visit user_path(current_user)
+      visit user_path(user)
       expect(page).to have_content("john@dbc.com")
+    end
+
+    it "prevents a logged in user from viewnig someone else's private info." do
+      user2 = User.create(first_name: "Jane", last_name: "Smith", email: "jane@dbc.com")      
+      visit user_path(user2)
+      expect(page).to have_no_content("jane@dbc.com")
     end
   end
 end
