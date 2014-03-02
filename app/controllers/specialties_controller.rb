bf9e3b6108b029721261df8c594210f2cec047f6
CharlieTruong <charlie.m.truong@gmail.com>
2014-03-02 17:29:28 -0600

    included conditional for when user removes all specialties

--- app/controllers/specialties_controller.rb
+++ app/controllers/specialties_controller.rb
@@ -1,8 +1,12 @@
 class SpecialtiesController < ApplicationController
   def create
     current_user.specialties.destroy_all
-    new_specialties = params['specialty'].keys
-    new_specialties.each{|specialty_id| UsersSpecialty.create(user_id: current_user.id, specialty_id: specialty_id)}
-    format.json{ render :json => {response: 'Specialties Updated'}}
+    if params['specialty']
+      new_specialties = params['specialty'].keys
+      new_specialties.each{|specialty_id| UsersSpecialty.create(user_id: current_user.id, specialty_id: specialty_id)}
+    end
+    respond_to do |format|
+      format.json{ render :json => {response: 'Specialties Updated'}}
+    end
   end
 end
