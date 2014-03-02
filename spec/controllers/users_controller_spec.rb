64b5dceb5c10d0ac910a330ef69ab5e8d473e29b
JuLu <junting.lu@gmail.com>
2014-03-02 09:06:46 -0600

    some tests, and setup some controllers

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../spec_helper'
+include OmniauthMacros
 
 # describe UsersController, :type => :controller do
 #   let(:user) {User.create(first_name: "John", last_name: "Smith", email: "john@dbc.com")}
@@ -14,5 +15,62 @@ require_relative '../spec_helper'
 
 describe UsersController do
 
+  describe 'Routing & Rendering' do
 
-end
+    describe 'GET#Index' do
+      it 'should render user index template' do
+        get :index
+        expect(response).to render_template :index
+      end
+    end
+
+    describe 'GET#Dashboard' do
+      it 'should render user dashboard template' do
+        user = FactoryGirl.create(:user)
+        login_user(user.id)
+
+        get :dashboard
+        expect(response).to render_template :dashboard
+      end
+    end
+
+    describe 'GET#Edit' do
+      it 'should render user edit template' do
+        user = FactoryGirl.create(:user)
+        login_user(user.id)
+
+        get :edit, {id: user.id}
+        expect(response).to render_template :edit
+      end
+    end
+
+    describe 'GET#Show' do
+      it 'should render user show template' do
+        user = FactoryGirl.create(:user)
+        login_user(user.id)
+
+        get :show, {id: user.id}
+        expect(response).to render_template :show
+      end
+    end
+
+  end #End Routing and Rendering
+
+  describe 'Object Population' do
+
+    describe 'users#ambassador_toggle' do
+      it 'should change the is_ambassador attribute' do
+        pending
+      end
+
+    end
+
+    describe 'users#ambassador_availability_toggle' do
+      it 'should change the ambassador_availability' do
+        pending
+      end
+    end
+
+  end # End Object Population
+
+end # End UsersController describe
