399485f56ceabb06965ec44017616f8f3b5a6766
JuLu <junting.lu@gmail.com>
2014-03-03 23:45:54 -0600

    breezy. ill leave the rest to you steven. :)

--- app/assets/javascripts/avatar_change_modal/fb_profile_modal.js
+++ app/assets/javascripts/avatar_change_modal/fb_profile_modal.js
@@ -16,12 +16,12 @@ $( document ).ready(function() {
           var image5 = response.data[4].images[0].source;
 
           vex.open({
-          content: '<h2>Pick a new profile picture!!!</h2>\
-                    <form action="/update_profile_pic/2" method="post"><input type="image" name="profile_pic" value="' + image1 + '" id="profile_image1" src="' + image1 + '" height="80" width="80" alt="Submit Form" /></form>\
-                    <form action="/update_profile_pic/2" method="post"><input type="image" name="profile_pic" value="' + image2 + '" id="profile_image2" src="' + image2 + '" height="80" width="80" alt="Submit Form" /></form>\
-                    <form action="/update_profile_pic/2" method="post"><input type="image" name="profile_pic" value="' + image3 + '" id="profile_image3" src="' + image3 + '" height="80" width="80" alt="Submit Form" /></form>\
-                    <form action="/update_profile_pic/2" method="post"><input type="image" name="profile_pic" value="' + image4 + '" id="profile_image4" src="' + image4 + '" height="80" width="80" alt="Submit Form" /></form>\
-                    <form action="/update_profile_pic/2" method="post"><input type="image" name="profile_pic" value="' + image5 + '" id="profile_image5" src="' + image5 + '" height="80" width="80" alt="Submit Form" /></form>'
+          content: '<h2 id="super_modal_h2">Pick a new profile picture!!!</h2>\
+                    <form action="/update_profile_pic/2" class="super_modal_form_styling" method="post"><input type="image" name="profile_pic" value="' + image1 + '" id="profile_image1" src="' + image1 + '" height="80" width="80" alt="Submit Form" /></form>\
+                    <form action="/update_profile_pic/2" class="super_modal_form_styling" method="post"><input type="image" name="profile_pic" value="' + image2 + '" id="profile_image2" src="' + image2 + '" height="80" width="80" alt="Submit Form" /></form>\
+                    <form action="/update_profile_pic/2" class="super_modal_form_styling" method="post"><input type="image" name="profile_pic" value="' + image3 + '" id="profile_image3" src="' + image3 + '" height="80" width="80" alt="Submit Form" /></form>\
+                    <form action="/update_profile_pic/2" class="super_modal_form_styling" method="post"><input type="image" name="profile_pic" value="' + image4 + '" id="profile_image4" src="' + image4 + '" height="80" width="80" alt="Submit Form" /></form>\
+                    <form action="/update_profile_pic/2" class="super_modal_form_styling" method="post"><input type="image" name="profile_pic" value="' + image5 + '" id="profile_image5" src="' + image5 + '" height="80" width="80" alt="Submit Form" /></form>'
           });
 
           });
