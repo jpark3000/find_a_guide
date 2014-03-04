64b76e904c025cb5f574390828d90b58fc0b3b9b
CharlieTruong <charlie.m.truong@gmail.com>
2014-03-04 15:57:44 -0600

    resolved date_time issue on meetup create

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -36,7 +36,8 @@ class MeetupsController < ApplicationController
   def create
     @ambassador = User.find(params[:ambassador_id])
     @visitor = User.find(params[:visitor_id])
-    meetup = Meetup.create(ambassador_id: @ambassador.id, visitor_id: @visitor.id, date_time: params[:start_date])
+    date = params[:start_date].split("/")
+    meetup = Meetup.create(ambassador_id: @ambassador.id, visitor_id: @visitor.id, date_time: DateTime.new(date[2], date[0], date[1]))
     subject = "#{@ambassador.name} will Meet You!"
     email_html = render_to_string "emails/accept", :layout => false
     Email.new_request(@ambassador, @visitor, email_html, subject)
