f5cd26117a45836c8d2155163b376684327cbf34
Steven Spiel <stevenspiel@gmail.com>
2014-03-05 10:49:16 -0600

    reposition zoom controls, add pin icon

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -91,7 +91,10 @@ $(document).ready(function() {
     streetViewControl: false,
     mapTypeControl: false,
     panControl: false,
-    zoomControlOptions: {style:google.maps.ZoomControlStyle.SMALL},
+    zoomControlOptions: {
+      style:google.maps.ZoomControlStyle.SMALL,
+      position: google.maps.ControlPosition.TOP_RIGHT
+    },
     styles: styleOptions
   };
 
