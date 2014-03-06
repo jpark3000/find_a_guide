760fb2b8c486391a143aa2a695ac3dc17afc58b3
JuLu <junting.lu@gmail.com>
2014-03-06 10:05:26 -0600

    update some seed data that probably wont be used

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,11 @@ s1 = Specialty.create!(specialty: "Culture")
 s2 = Specialty.create!(specialty: "Nightlife")
 s3 = Specialty.create!(specialty: "Food and Drink")
 
-Specialty.create!(specialty: "Special Events")
-Specialty.create!(specialty: "Art"
-Specialty.create!(specialty: "Adventure")
-Specialty.create!(specialty: "Navigator")
-Specialty.create!(specialty: "Music")
+# Specialty.create!(specialty: "Special Events")
+# Specialty.create!(specialty: "Art"
+# Specialty.create!(specialty: "Adventure")
+# Specialty.create!(specialty: "Navigator")
+# Specialty.create!(specialty: "Music")
 
 l1 = Language.create!(language: "English")
 l2 = Language.create!(language: "Spainish")
@@ -26,7 +26,7 @@ l4 = Language.create!(language: "German")
 gender = ['male','female']
 boolean = [true,false]
 
-random_uid = ['511571783', '724578054', '1037742890', '100000287146443', '100001318974142', '1632193255', '568774194', '100002092401128', '100001847404308']
+random_uid = ['511571783', '724578054', '1037742890', '100000287146443', '100001318974142', '1632193255', '568774194', '100002092401128', '100001847404308', '812240594', '756515675', '739760365', '205400447', '100000615675453', '661644102', '6025069', '667347118', '205400014', '775530383', '205400045', '645635108', '100001708508222', '205400469', '1211270', '205400552', '763415440', '205400767', '100000200855790', '692761059', '13309604']
 
 50.times do
   User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,
@@ -71,3 +71,23 @@ User.all.each do |u|
     end
   end
 end
+
+# Real person seed data
+
+# real_person_uid = ['511571783', '724578054', '1037742890', '100000287146443', '100001318974142', '1632193255', '568774194', '100002092401128', '100001847404308', '812240594', '756515675', '739760365', '205400447', '100000615675453', '661644102', '6025069', '667347118', '205400014', '775530383', '205400045', '645635108', '100001708508222', '205400469', '1211270', '205400552', '763415440', '205400767', '100000200855790', '692761059', '13309604']
+
+
+# real_person_uid.each do |uid|
+#   User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,
+#                email: Faker::Internet.email, bio: Faker::Lorem.sentences.join(" "),
+#                gender: gender.sample, phone: Faker::PhoneNumber.cell_phone, age: (18..60).to_a.sample,
+#                tagline: Faker::Commerce.product_name, is_ambassador: true, uid: uid, profile_pic: "http://graph.facebook.com/#{uid}/picture?type=large").save(validate: false)
+# end
+
+# 250.times do
+#     User.all.sample.tours.create!(longitude: Faker::Address.longitude,
+#                        latitude: Faker::Address.latitude,
+#                        description: Faker::Lorem.paragraphs.join("\n\n"))
+# end
+
+
